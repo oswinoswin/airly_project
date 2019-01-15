b79280fc39b09237b07af3d9ef66ed5fe1839215
oswinoswin <muchwat@agh.edu.pl>
2019-01-15 14:42:25 +0100

    add cleaned data analysis

--- plot_by_hour.py
+++ plot_by_hour.py
@@ -75,21 +75,21 @@ def plot_points(points, triangles, labels, title, path):
 
     fig1, ax1 = plt.subplots()
     ax1.set_aspect('equal')
-    tpc = ax1.tripcolor(triang, facecolors=labels, edgecolors='k')
+    tpc = ax1.tripcolor(triang, facecolors=labels, edgecolors='k', vmin=0, vmax=40)
     fig1.colorbar(tpc)
     plt.title(title)
     # plt.show()
     plt.savefig(path)
 
 
-parameter  = 'pm10'
+parameter = 'pm1'
 
 for start_hour, end_hour in zip(range(0,23), range(1,24)):
     locations = fetch_data_for_locations_by_hours(start_hour, end_hour, parameter)
     points, triangles, avg = prepare_points_triangles_avg(locations)
     labels = np.array([ np.mean(row)  for row in np.nan_to_num(avg)])
-    title = f'{parameter} Mean at {start_hour}-{end_hour} hour (October/November 2018)'
-    path = f'plots/pm10_mean_by_hour/{parameter}_by_hour_{start_hour}_{end_hour}_mean.png'
+    title = f'{parameter} Mean at {start_hour}-{end_hour} hour (October-December 2018)'
+    path = f'plots/{parameter}_mean_by_hour/{parameter}_by_hour_{start_hour}_{end_hour}_mean.png'
     plot_points(points, triangles, labels, title, path)
 
 # locations = fetch_data_for_locations_by_hours(start_hour, end_hour, parameter)
